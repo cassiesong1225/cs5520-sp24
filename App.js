559dcfa413e635d2c1032cbd2943d9e66c42bccd
Cassie Song <cassiesong@haidegongyuanxiaogezideMacBook-Air.local>
2024-01-27 13:03:45 -0800

    week3 activites

--- App.js
+++ App.js
@@ -1,20 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, TextInput, View, Text, Button, SafeAreaView } from "react-native";
+import Header from "./components/Header";
+import { useState } from "react";
+import Input from "./components/Input";
 
 export default function App() {
+  const appName = "My awesome app";
+  const [text, setText] = useState("");
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  function receiveInput(data) { 
+    console.log("recevie Input", data);
+    setText(data);
+    setIsModalVisible(false);
+  }
+  function dismissModal() {
+    setIsModalVisible(false);
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+    <View style={styles.topView}>
       <StatusBar style="auto" />
-    </View>
+      <Header name={appName} version={2} />
+      <Button title = "Add a goal" onPress={() => setIsModalVisible(true)} />
+      <Input inputHandler={receiveInput} modalVisible={isModalVisible} dismissModal={ dismissModal} />
+      </View>
+      <View style={styles.bottomView}>
+        <Text style={styles.text}>{text}</Text> 
+      </View>
+      </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "white",
+    // alignItems: "center",
+    justifyContent: "center",
   },
+  topView: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "space-around",
+  },
+  bottomView: { flex: 4, backgroundColor: "lightpink" },
+  text: { textAlign: "center" },
 });
